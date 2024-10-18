0f1c86d43136a3f901b1893da6b24ea24b6d5c60
Oliver Hughes <oliver.hughes@adevinta.com>
2024-10-19 01:10:21 +0200

    Sort file paths before building sitemap

--- internal/common/sitemap.go
+++ internal/common/sitemap.go
@@ -3,20 +3,34 @@ package common
 import (
 	"fmt"
 	"path/filepath"
+	"slices"
 	"strings"
 )
 
 func BuildSitemap(domain string, pageMap *PageMap) string {
 	var builder strings.Builder
+	sortedPaths := sortByPath(pageMap)
+
 	builder.WriteString(`<?xml version="1.0" encoding="UTF-8"?>`)
 	builder.WriteString(`<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">`)
-	for path := range *pageMap {
+	for _, path := range sortedPaths {
 		urlPath := fileToUrlPath(path)
 		builder.WriteString(fmt.Sprintf("<url><loc>https://%s%s</loc></url>", domain, urlPath))
 	}
 	builder.WriteString("</urlset>")
 
 	return builder.String()
+}
+
+func sortByPath(pageMap *PageMap) []string {
+	keys := make([]string, len(*pageMap))
+	i := 0
+	for k := range *pageMap {
+		keys[i] = k
+		i++
+	}
+	slices.Sort(keys)
+	return keys
 }
 
 func fileToUrlPath(path string) string {
